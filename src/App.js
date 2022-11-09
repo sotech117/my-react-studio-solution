126ca70b8b36454292fb8812862c3e296f66c547
Michael Foiani <sotech117@michaels-mbp-21.devices.brown.edu>
2022-11-09 12:23:16 -0500

    finish

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,18 +12,33 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+    const [cart, setCart] = useState([]);
+    const [totalPrice, setPrice] = useState(0);
+
+    const updatePrice = () => {
+        let sum = 0;
+        cart.forEach(i => sum += i.price);
+        setPrice(sum)
+    }
+
+    useEffect(() => {
+        updatePrice();
+    }, [cart]);
 
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+          <BakeryItem item={item} cart={cart}
+                      updateCart={setCart}/>
+        //<p>Bakery Item {index}</p> // replace with BakeryItem component
       ))}
 
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        {cart.map(e => <p>{e.name}</p>)}
+          <p>{totalPrice}</p>
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,22 @@
 // TODO: create a component that displays a single bakery item
+export default function BakeryItem(props) {
+    // const item = props.item;
+    // const updateCart = props.updateCart;
+    // const cart = props.cart;
+    const {item, updateCart, cart} = props;
+    const handleClick = () => {
+        // do stuff
+        console.log(`clicked ${item.name}`)
+        updateCart([...cart, item]);
+    }
+
+    return (
+        <div>
+            <h3>{item.name}</h3>
+            <p>{item.description}</p>
+            <button onClick={handleClick}>click me</button>
+        </div>
+    );
+}
+
+// SUBMIT: tinyurl.com/mc-react
